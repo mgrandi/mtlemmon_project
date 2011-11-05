f6f170d747206720bc99eaed81e14dcdca346253
Mark Grandi <markgrandi@gmail.com>
2011-11-05 15:00:41 -0700

    renamed the app delegate files and whatnot

---
+++ mtlemmon_project/MtlemmonAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mtlemmon_project
+//
+//  Created by Mark Grandi on 11/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MtlemmonAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
