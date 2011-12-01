0b61fa7cc24038635ceec84f2a8d54742208b2ad
Mark Grandi <markgrandi@gmail.com>
2011-12-01 15:01:40 -0700

    uhhh why did this remove from the head revision

--- mtlemmon_project/InspirationRockViewController.m
+++ mtlemmon_project/InspirationRockViewController.m
@@ -18,14 +18,67 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-
+        
+        UIImage *bg = [UIImage imageNamed:@"background_1.png"];
+        UIImageView *bgv = [[UIImageView alloc] initWithImage: bg];
+        [self.view addSubview: bgv];
+        
+        
         // add the button to the top right of the navigation bar to take a journal entry
         UIBarButtonItem *journalButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose target:self action:@selector(journalButtonClicked:)];
         
         [[self navigationItem] setRightBarButtonItem:journalButton];
         
         [journalButton release];
-    
+        
+        // *********************************************
+        // CreateWelcome Text
+        // *********************************************
+        UILabel *welcome = [[UILabel alloc] init ];
+        welcome.numberOfLines = 0; // Allows for infinite number of lines
+        welcome.lineBreakMode = UILineBreakModeWordWrap;
+        [welcome setText: @"Welcome to Stop 4: \nInspiration Rock! \n\n\n\nWhat would you like to learn about?"];
+        welcome.backgroundColor = [UIColor clearColor];
+        welcome.textColor = [UIColor whiteColor];
+        welcome.frame = CGRectMake(10.0, 20.0, [UIScreen mainScreen].bounds.size.width - 20.0, 130.0);
+        [self.view addSubview: welcome];
+        
+        
+        // *********************************************
+        // Create Life Zones Button
+        // *********************************************
+        UIButton *buttonLZ = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [buttonLZ addTarget:self 
+                     action:@selector(lZPressed:)
+           forControlEvents:UIControlEventTouchDown];
+        [buttonLZ setTitle:@"Life Zones" forState:UIControlStateNormal];
+        [buttonLZ setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
+        buttonLZ.frame = CGRectMake(20.0, 160.0, [UIScreen mainScreen].bounds.size.width - 40.0 , 35.0);
+        [self.view addSubview:buttonLZ];
+        
+        // *********************************************
+        // Create Fire Button
+        // *********************************************
+        UIButton *buttonFire = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [buttonFire addTarget:self 
+                     action:@selector(firePressed:)
+           forControlEvents:UIControlEventTouchDown];
+        [buttonFire setTitle:@"Fire" forState:UIControlStateNormal];
+        [buttonFire setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+        buttonFire.frame = CGRectMake(20.0, 215.0, [UIScreen mainScreen].bounds.size.width - 40.0 , 35.0);
+        [self.view addSubview:buttonFire];
+        
+        // *********************************************
+        // Create Geology Button
+        // *********************************************
+        UIButton *buttonGeo = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [buttonGeo addTarget:self 
+                      action:@selector(geoPressed:)
+            forControlEvents:UIControlEventTouchDown];
+        [buttonGeo setTitle:@"Geology" forState:UIControlStateNormal];
+        [buttonGeo setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        buttonGeo.frame = CGRectMake(20.0, 270.0, [UIScreen mainScreen].bounds.size.width - 40.0 , 35.0);
+        [self.view addSubview:buttonGeo];
     }
     return self;
 }
--- mtlemmon_project/MolinoCanyonViewController.m
+++ mtlemmon_project/MolinoCanyonViewController.m
@@ -17,15 +17,59 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-
+        
+        UIImage *bg = [UIImage imageNamed:@"background_1.png"];
+        UIImageView *bgv = [[UIImageView alloc] initWithImage: bg];
+        [self.view addSubview: bgv];
+        
+        
+        
+        
         // add the button to the top right of the navigation bar to take a journal entry
         UIBarButtonItem *journalButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCompose target:self action:@selector(journalButtonClicked:)];
         
         [[self navigationItem] setRightBarButtonItem:journalButton];
         
         [journalButton release];
-    
-    
+        
+        // *********************************************
+        // Create Welcome Text
+        // *********************************************
+        UILabel *welcome = [[UILabel alloc] init ];
+        welcome.numberOfLines = 0; // Allows for infinite number of lines
+        welcome.lineBreakMode = UILineBreakModeWordWrap;
+        [welcome setText: @"Welcome to Stop 1: \nMolino Canyon Overlook! \n\n\n\nWhat would you like to learn about?"];
+        welcome.backgroundColor = [UIColor clearColor];
+        welcome.textColor = [UIColor whiteColor];
+        welcome.frame = CGRectMake(10.0, 20.0, [UIScreen mainScreen].bounds.size.width - 20.0, 130.0);
+        [self.view addSubview: welcome];
+        
+        // *********************************************
+        // Create Geo History Button
+        // *********************************************
+        UIButton *buttonGeo = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [buttonGeo addTarget:self 
+                       action:@selector(geoPressed:)
+             forControlEvents:UIControlEventTouchDown];
+        [buttonGeo setTitle:@"Geo History" forState:UIControlStateNormal];
+        [buttonGeo setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        buttonGeo.frame = CGRectMake(20.0, 160.0, [UIScreen mainScreen].bounds.size.width - 40.0 , 35.0);
+        [self.view addSubview:buttonGeo];
+        
+        
+        
+        
+        // *********************************************
+        // Create Life Zones Button
+        // *********************************************
+        UIButton *buttonLZ = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [buttonLZ addTarget:self 
+                     action:@selector(lZPressed:)
+           forControlEvents:UIControlEventTouchDown];
+        [buttonLZ setTitle:@"Life Zones" forState:UIControlStateNormal];
+        [buttonLZ setTitleColor:[UIColor greenColor] forState:UIControlStateNormal];
+        buttonLZ.frame = CGRectMake(20.0, 215.0, [UIScreen mainScreen].bounds.size.width - 40.0 , 35.0);
+        [self.view addSubview:buttonLZ];
     }
     return self;
 }
