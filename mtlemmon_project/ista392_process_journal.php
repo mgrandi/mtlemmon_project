5fbea75b6344fa43e0b2949f55d1506e34fe9845
Mark Grandi <markgrandi@gmail.com>
2011-12-09 11:55:02 -0700

    edited the php file a bit so it saves the image from the json if it got one.

--- mtlemmon_project/ista392_process_journal.php
+++ mtlemmon_project/ista392_process_journal.php
@@ -3,7 +3,26 @@
 
     $request_body = file_get_contents('php://input');
 
-    file_put_contents("ista392_jsondump_data_" . time() . ".txt", $request_body);
+    $jsonArray = json_decode($request_body, true);
+
+
+    if ($jsonArray != NULL) {
+        
+        if (array_key_exists("image", $jsonArray[0])) {
+            
+
+             $image = $jsonArray[0]["image"];
+
+
+             $resultImage = base64_decode($image);
+
+             file_put_contents("ista392_process_journal_image " . time() . ".png", $resultImage);
+
+        }
+
+    }
+
+    file_put_contents("ista392_process_journal_jsondump_data_" . time() . ".json", $request_body);
     
     echo("Server says: Got it!");
 
